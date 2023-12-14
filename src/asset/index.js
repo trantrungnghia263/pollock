8f9abbe93ff2c30857431c2179ad392430a1cf30
Your Name <you@example.com>
2023-12-14 17:35:59 +0700

    upload

--- src/asset/index.js
+++ src/asset/index.js
@@ -1,37 +1,37 @@
 document.addEventListener('DOMContentLoaded',function(){
     // code toggle === Monthly === //
-    // const pricingClickElement = document.querySelector('#page-pricing__click');
-    // const label = document.querySelector('.page-pricing__label');
-    // const brg = document.querySelector('.page-pricing__toggle');
+    const pricingClickElement = document.querySelector('#page-pricing__click');
+    const label = document.querySelector('.page-pricing__label');
+    const brg = document.querySelector('.page-pricing__toggle');
     
-    // pricingClickElement.addEventListener('change', function() {
-    //     if (this.checked) {
-    //         label.classList.add('active');
-    //         brg.classList.add('active');
-    //     } else {
-    //         label.classList.remove('active');
-    //         brg.classList.remove('active');
-    //     }
-    // });
+    pricingClickElement.addEventListener('change', function() {
+        if (this.checked) {
+            label.classList.add('active');
+            brg.classList.add('active');
+        } else {
+            label.classList.remove('active');
+            brg.classList.remove('active');
+        }
+    });
 
-    // // code slide //
-    // const texts = document.querySelectorAll(".page-about__text");
-    // const dots = document.querySelectorAll(".page-about__dot");
+    // code slide //
+    const texts = document.querySelectorAll(".page-about__text");
+    const dots = document.querySelectorAll(".page-about__dot");
 
-    // dots.forEach((dot,index)=>{
-    //     dot.addEventListener('click',()=>{
-    //         // xoá 'active' class từ tất cả các texts và dots
-    //         texts.forEach(text=>{
-    //             text.classList.remove('active');
-    //         })
-    //         dots.forEach(dot=>{
-    //             dot.classList.remove('active');
-    //         })
-    //         // thêm 'active' class để click dots và texts tương ứng
-    //         dot.classList.add('active');
-    //         texts[index].classList.add('active');
-    //     })
-    // });
+    dots.forEach((dot,index)=>{
+        dot.addEventListener('click',()=>{
+            // xoá 'active' class từ tất cả các texts và dots
+            texts.forEach(text=>{
+                text.classList.remove('active');
+            })
+            dots.forEach(dot=>{
+                dot.classList.remove('active');
+            })
+            // thêm 'active' class để click dots và texts tương ứng
+            dot.classList.add('active');
+            texts[index].classList.add('active');
+        })
+    });
 
     // dropdown
     document.addEventListener('DOMContentLoaded', function () {
